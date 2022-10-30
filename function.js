5ec86cd7b8ea86e87eaaa05f69183ea48b2d09c5
javierch2022 <javierchavarri@tdf.edu.ar>
2022-10-30 20:32:53 -0300

    20:32

--- function.js
+++ function.js
@@ -4,55 +4,52 @@ const apiClima = "https://api.open-meteo.com/v1/forecast?latitude=-54.82&longitu
 
 
 fetch(apiClima)
-        .then(response => response.json())
-        .then(data => {
+   .then(response => response.json())
+   .then(data => {
+      // TEST : llamados de lista json
+      /*console.log(data.hourly.time[0]);
+      console.log(data.hourly.temperature_2m[0]);
+      console.log(data.hourly.apparent_temperature[0]);
+      console.log(data.hourly.precipitation[0]);
+      console.log(data.hourly.windspeed_10m[0]);*/
 
-                console.log(data.hourly.time[0]);
-                console.log(data.hourly.temperature_2m[0]);
-                console.log(data.hourly.apparent_temperature[0]);
-                console.log(data.hourly.precipitation[0]);
-                console.log(data.hourly.windspeed_10m[0]);
+      // variable es para la hora dia actual
+      let horaActual = new Date;
 
-
-                let horaActual = new Date;
-                
-                console.log(horaActual)
-                //este bloque para el dia y hora
-                let dia = document.getElementById('dia')
-                dia.innerHTML = `
+      // TEST:console.log(horaActual);
+      //este bloque para el dia y hora
+      let dia = document.getElementById('dia')
+      dia.innerHTML = `
                 <div >
                    <h5>${horaActual}</h5>
                 </div> `;
-                // este bloque para el numbero de temperatura
-                let temperatura = document.getElementById('temp')
-                temperatura.innerHTML = `
-                 <div id="temp1">
-                    <h3>Temperatura</h3>
-                    <h1>${data.hourly.temperature_2m[0]}</h1>
+      // este bloque para el numbero de temperatura
+      let temperatura = document.getElementById('temp')
+      temperatura.innerHTML = `
+                 <div>
+                    <h2>Temperatura</h2>
+                    <h1 id=temp1>${data.hourly.temperature_2m[0]}°</h1>
                  </div> `;
-                // este bloque para la condicion del dia
-                let condicion = document.getElementById('condicion')
-                condicion.innerHTML = `
+      // este bloque para la condicion del dia
+      let condicion = document.getElementById('condicion')
+      condicion.innerHTML = `
                   <div> 
                     <h3>Sensacion Termica</h3>
-                     <h1 id="condicion1">${data.hourly.apparent_temperature[0]}</h1>
+                     <h1 id="condicion1">${data.hourly.apparent_temperature[0]} °</h1>
                   </div>
                   <div> 
                      <h3>Precipitacion</h3>
-                     <h1 id="condicion1">${data.hourly.precipitation[0]}</h1>
+                     <h1 id="condicion1">${data.hourly.precipitation[0]} %</h1>
                   </div> 
                   <div> 
                      <h3>Velocidad del Viento</h3>
-                     <h1 id="condicion1">${data.hourly.windspeed_10m[0]}</h1>
+                     <h1 id="condicion1">${data.hourly.windspeed_10m[0]} Km/h</h1>
                   </div>`;
-                   // este bloque para la imagen
-                let imagen = document.getElementById('imagen')
-                imagen.innerHTML = `
+      // este bloque para la imagen
+      let imagen = document.getElementById('imagen')
+      imagen.innerHTML = `
                   <div >
                      <img id= "imagen1" src="../img/icon/animated/cloudy-2-day.svg">
                   </div> `
-        
-        });
 
-
-        
+   });
