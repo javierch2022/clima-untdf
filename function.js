b242e016cb587bb95a4794afef629b0f440706ce
Daniel Barrera <danub25@gmail.com>
2022-11-06 13:33:02 -0300

    agrego comentarios a las funciones

--- function.js
+++ function.js
@@ -3,13 +3,11 @@ const apiClima = "https://api.open-meteo.com/v1/forecast?latitude=-54.82&longitu
 function buscarPosicion(data, fecha) {
    let pos = 0
    pos = data.hourly.time.indexOf(fecha);
-
    return pos
 }
 
 function selectImagenClima(data, fecha) {
    let ubicacion = "";
-   console.log("data en select img", data)
    let posicion = buscarPosicion(data, fecha);
    let lluvia = data.hourly.precipitation[posicion];
 
@@ -54,32 +52,34 @@ function selectImagenClima(data, fecha) {
 function llenarTabla(data, fecha) {
    let posicion = buscarPosicion(data, fecha);
    let datos = data.hourly;
+   //cambio posicion al inicio del dia correspondiente
    switch (posicion) {
-      case (posicion > 12) & (posicion < 24):
+      case (posicion > 12) && (posicion < 24):
          posicion = 13;
          break;
-      case (posicion > 23) & (posicion < 48):
+      case (posicion > 23) && (posicion < 48):
          posicion = 25
          break;
-      case (posicion > 47) & (posicion < 72):
+      case (posicion > 47) && (posicion < 72):
          posicion = 49;
          break;
-      case (posicion > 71) & (posicion < 96):
+      case (posicion > 71) && (posicion < 96):
          posicion = 73;
          break;
-      case (posicion > 95) & (posicion < 120):
+      case (posicion > 95) && (posicion < 120):
          posicion = 97;
          break;
-      case (posicion > 119) & (posicion < 144):
+      case (posicion > 119) && (posicion < 144):
          posicion = 121;
          break;
-      case (posicion > 143) & (posicion <= 168):
+      case (posicion > 143) && (posicion <= 168):
          posicion = 145;
          break;
       default:
          posicion = 0;
    }
    let hora = 0;
+   //lleno la tabla
    let tabla = document.getElementById('tabla');
    for (i = 1; i < 9; i++) {
       tabla.innerHTML += `
@@ -91,6 +91,7 @@ function llenarTabla(data, fecha) {
          <td>${datos.precipitation[posicion]}</td>
          <td>${datos.windspeed_10m[posicion]}</td>
    </tr>`
+      //voy cambiando la hora y la posicion para tomar los datos siguientes (cada 3 hs)
       hora += 3;
       posicion += 3;
    }
@@ -99,6 +100,7 @@ function llenarTabla(data, fecha) {
 }
 
 function datosEnCard(data, fecha) {
+   //obtengo el string de mañana
    const dias = [
       'domingo',
       'lunes',
@@ -110,25 +112,33 @@ function datosEnCard(data, fecha) {
    ];
    let numeroDia = new Date(fecha).getDay();
    let dia = dias[numeroDia];
+   //consigo la posicion de mañana 
    let dato = data.hourly;
-   let posicion = buscarPosicion(data, fecha)+24;
-   let imagen = selectImagenClima(data, fecha);
-
+   let posicion = buscarPosicion(data, fecha) + 24;
+   // creo las cards de los dias siguientes
    let card = document.getElementById('section3');
+   
+   let idcard="card";
+   
    for (i = 1; i < 7; i++) {
-      
+      idcard=idcard+toString(i);
+      //cambio la fecha al dia siguiente para seleccionar la imagen
+      fecha=dato.time[posicion+24];
+      let imagen = selectImagenClima(data, fecha);
+      //cards
       card.innerHTML += `        
       <div id= "card" class="card" type="button" value="lunes">
                   <div class="card-body">
                       <h5 class="card-title">${dia}</h5><br>
                       <img id= "test" src=${imagen} class="card-img-top" alt="...">
                       <h1>${dato.temperature_2m[posicion]}</h1>
+                      
                     </div>
       </div>
                     `
-      
-      posicion=posicion+24;
-      numeroDia=numeroDia+1;
+      //cambio la posicion 24 hs y paso al dia siguiente
+      posicion += 24;
+      numeroDia += 1;
       dia = dias[numeroDia];
    }
    return
@@ -169,8 +179,6 @@ function climaHoy(data, fecha) {
    // este bloque para la imagen
    console.log("data info: ", data);
    let ubicacion = selectImagenClima(data, fecha);
-
-   //let ubicacion = "../img/icon/animated/rainy-1.svg";
    let imagen = document.getElementById('imagen')
    imagen.innerHTML = `
                   <div >
@@ -180,7 +188,10 @@ function climaHoy(data, fecha) {
    return
 }
 
-
+function eventoClick(data,fecha){
+   llenarTabla(data,fecha);
+}
+const card1= document.getElementById('')
 fetch(apiClima)
    .then(response => response.json())
    .then(data => {
@@ -189,4 +200,5 @@ fetch(apiClima)
       console.log(ahora);
       climaHoy(data, ahora);
       datosEnCard(data, ahora);
+
    });
